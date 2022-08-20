acd44a75fe2545d732a4dcd0f4a11983b522ec29
xiguatianbutian <fyq081010@163.com>
2022-08-20 12:48:41 +0800

    Delet some Modules

---
+++ shared/main/java/net/ccbluex/liquidbounce/features/module/ModuleManager.kt
@@ -0,0 +1,203 @@
+/*
+ * LiquidBounce Hacked Client
+ * A free open source mixin-based injection hacked client for Minecraft using Minecraft Forge.
+ * https://github.com/CCBlueX/LiquidBounce/
+ */
+package net.ccbluex.liquidbounce.features.module
+
+import net.ccbluex.liquidbounce.LiquidBounce
+import net.ccbluex.liquidbounce.event.EventTarget
+import net.ccbluex.liquidbounce.event.KeyEvent
+import net.ccbluex.liquidbounce.event.Listenable
+import net.ccbluex.liquidbounce.features.module.modules.`fun`.Derp
+import net.ccbluex.liquidbounce.features.module.modules.combat.*
+import net.ccbluex.liquidbounce.features.module.modules.exploit.*
+import net.ccbluex.liquidbounce.features.module.modules.misc.*
+import net.ccbluex.liquidbounce.features.module.modules.movement.*
+import net.ccbluex.liquidbounce.features.module.modules.player.*
+import net.ccbluex.liquidbounce.features.module.modules.render.*
+import net.ccbluex.liquidbounce.features.module.modules.world.*
+import net.ccbluex.liquidbounce.utils.ClientUtils
+import java.util.*
+
+
+class ModuleManager : Listenable {
+
+    val modules = TreeSet<Module> { module1, module2 -> module1.name.compareTo(module2.name) }
+    private val moduleClassMap = hashMapOf<Class<*>, Module>()
+
+    init {
+        LiquidBounce.eventManager.registerListener(this)
+    }
+
+    /**
+     * Register all modules
+     */
+    fun registerModules() {
+        ClientUtils.getLogger().info("[ModuleManager] Loading modules...")
+
+        registerModules(
+                AutoArmor::class.java,
+                BowAimbot::class.java,
+                Criticals::class.java,
+                KillAura::class.java,
+                Fly::class.java,
+                ClickGUI::class.java,
+                InventoryMove::class.java,
+                NoSlow::class.java,
+                LiquidWalk::class.java,
+                Sprint::class.java,
+                Teams::class.java,
+                Scaffold::class.java,
+                CivBreak::class.java,
+                Tower::class.java,
+                FastBreak::class.java,
+                FastPlace::class.java,
+                ESP::class.java,
+                Speed::class.java,
+                Tracers::class.java,
+                NameTags::class.java,
+                Fullbright::class.java,
+                ItemESP::class.java,
+                StorageESP::class.java,
+                Nuker::class.java,
+                FastClimb::class.java,
+                AutoTool::class.java,
+                Spammer::class.java,
+                NoFall::class.java,
+                Blink::class.java,
+                NameProtect::class.java,
+                NoHurtCam::class.java,
+                XRay::class.java,
+                Sneak::class.java,
+                GhostHand::class.java,
+                FreeCam::class.java,
+                HitBox::class.java,
+                AntiHunger::class.java,
+                FastBow::class.java,
+                MultiActions::class.java,
+                AirJump::class.java,
+                AutoClicker::class.java,
+                NoBob::class.java,
+                BlockOverlay::class.java,
+                NoFriends::class.java,
+                BlockESP::class.java,
+                Chams::class.java,
+                Phase::class.java,
+                NoFOV::class.java,
+                SwingAnimation::class.java,
+                Derp::class.java,
+                TrueSight::class.java,
+                LiquidChat::class.java,
+                AntiBlind::class.java,
+                NoSwing::class.java,
+                Breadcrumbs::class.java,
+                AbortBreaking::class.java,
+                PotionSaver::class.java,
+                CameraClip::class.java,
+                NoPitchLimit::class.java,
+                Liquids::class.java,
+                AirLadder::class.java,
+                ProphuntESP::class.java,
+                Reach::class.java,
+                Rotations::class.java,
+                NoJumpDelay::class.java,
+                HUD::class.java,
+                TNTESP::class.java,
+                ComponentOnHover::class.java,
+                NoSlowBreak::class.java,
+                PortalMenu::class.java
+        )
+
+        registerModule(NoScoreboard)
+        registerModule(Fucker)
+        registerModule(ChestAura)
+        registerModule(AntiBot)
+
+        ClientUtils.getLogger().info("[ModuleManager] Loaded ${modules.size} modules.")
+    }
+
+    /**
+     * Register [module]
+     */
+    fun registerModule(module: Module) {
+        if (!module.isSupported)
+            return
+
+        modules += module
+        moduleClassMap[module.javaClass] = module
+
+        generateCommand(module)
+        LiquidBounce.eventManager.registerListener(module)
+    }
+
+    /**
+     * Register [moduleClass]
+     */
+    private fun registerModule(moduleClass: Class<out Module>) {
+        try {
+            registerModule(moduleClass.newInstance())
+        } catch (e: Throwable) {
+            ClientUtils.getLogger().error("Failed to load module: ${moduleClass.name} (${e.javaClass.name}: ${e.message})")
+        }
+    }
+
+    /**
+     * Register a list of modules
+     */
+    @SafeVarargs
+    fun registerModules(vararg modules: Class<out Module>) {
+        modules.forEach(this::registerModule)
+    }
+
+    /**
+     * Unregister module
+     */
+    fun unregisterModule(module: Module) {
+        modules.remove(module)
+        moduleClassMap.remove(module::class.java)
+        LiquidBounce.eventManager.unregisterListener(module)
+    }
+
+    /**
+     * Generate command for [module]
+     */
+    internal fun generateCommand(module: Module) {
+        val values = module.values
+
+        if (values.isEmpty())
+            return
+
+        LiquidBounce.commandManager.registerCommand(ModuleCommand(module, values))
+    }
+
+    /**
+     * Legacy stuff
+     *
+     * TODO: Remove later when everything is translated to Kotlin
+     */
+
+    /**
+     * Get module by [moduleClass]
+     */
+    fun getModule(moduleClass: Class<*>) = moduleClassMap[moduleClass]!!
+
+    operator fun get(clazz: Class<*>) = getModule(clazz)
+
+    /**
+     * Get module by [moduleName]
+     */
+    fun getModule(moduleName: String?) = modules.find { it.name.equals(moduleName, ignoreCase = true) }
+
+    /**
+     * Module related events
+     */
+
+    /**
+     * Handle incoming key presses
+     */
+    @EventTarget
+    private fun onKey(event: KeyEvent) = modules.filter { it.keyBind == event.key }.forEach { it.toggle() }
+
+    override fun handleEvents() = true
+}
